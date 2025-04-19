d99943e0994a0dec524ba7aa9d0a748443a938fe
janebv15 <jvaughn0@chicagobooth.edu>
2025-04-19 23:59:12 +0000

    setup

--- app.rb
+++ app.rb
@@ -2,8 +2,42 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:homepage)
+
 end
+
+get("/rock") do
+moves = ["Rock", "Paper", "Scissors"]
+@comp_move = moves.sample
+
+if @comp_move == "rock"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @outcome = "lost"
+else
+  @outcome = "won"
+end
+erb(:zebra)
+end
+
+get("/paper") do
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:poppy)
+  end
+
+  get("/scissors") do
+    if @comp_move == "scissors"
+      @outcome = "tied"
+    elsif @comp_move == "rock"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    erb(:shine)
+    end
